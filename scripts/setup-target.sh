03188f91fb6dd0293c1e62c75a11a2bc93cf3301
Veronika Dunaeva <dunaevaveronika13@gmail.com>
2026-05-25 14:03:49 +0300

    fixed setup script

--- scripts/setup-target.sh
+++ scripts/setup-target.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Встановлення Docker..."
+sudo apt-get update
+sudo apt-get install -y curl git
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
-echo "Налаштування прав SSH..."
+sudo apt-get install -y docker-compose-plugin
+
 echo "HostKeyAlgorithms +*" | sudo tee -a /etc/ssh/sshd_config
 echo "PubkeyAcceptedAlgorithms +*" | sudo tee -a /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
 sudo mkdir -p /opt/mywebapp
-sudo chown -R $USER:$USER /opt/mywebapp
+sudo chown -R "$USER":"$USER" /opt/mywebapp
 
-echo "Сервер налаштовано"
+echo "Сервер налаштовано успішно"
